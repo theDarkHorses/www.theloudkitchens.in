b82ed599bccff827a0bda106d38e9cf62e4c5cf6
rohit gupta <lucidrohit@gmail.com>
2023-10-11 22:25:59 +0530

    added home route and restaurant card

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,7 +9,7 @@ const navLinks = [
         label: "Home",
         icon: "/icons/homeOutline.png",
         activeIcon: "/icons/home.png",
-        pathName: "/"
+        pathName: "/home"
     },
     {
         label: "Search",
@@ -44,10 +44,10 @@ const navLinks = [
 export default function Footer() {
     const pathName = usePathname()
     return (
-        <footer className='w-full h-16 border absolute bottom-0 left-0 right-0 shadow-sm shadow-slate-600 rounded-t-md'>
+        <footer className='h-16 border absolute bottom-0 left-0 right-0 w-screen shadow-sm shadow-slate-600 rounded-t-md'>
             <ul className="flex gap-2 items-center justify-center h-full">
                 {navLinks.map(item =>
-                    <Link href={item.pathName} className="flex-1  border-primary gap-2 flex flex-col items-center justify-center">
+                    <Link href={item.pathName} key={item.pathName} className="flex-1  border-primary gap-2 flex flex-col items-center justify-center">
                         <Image src={pathName == item.pathName ? item.activeIcon : item.icon} width={20} height={20} />
                         <p className={`capitalize text-yash  font-raleway ${pathName == item.pathName ? "text-primary font-semibold" : "text-text"}`}>{item.label}</p>
                     </Link>
