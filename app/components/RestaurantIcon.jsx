cdeea3457c0895338bc36f8d336b08f4c9ee4372
rohit gupta <lucidrohit@gmail.com>
2023-10-12 20:42:44 +0530

    added carousel

--- app/components/RestaurantIcon.jsx
+++ app/components/RestaurantIcon.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import React from "react";
 
 const RestaurantIcon = ({ isActive, imgSrc, name }) => {
-  console.log(name);
   return (
     <Link href={"/restaurants"} className="flex first-of-type:ml-5 flex-col mt-1 ml-2">
       <div className="h-[82px] w-[82px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl flex items-center justify-center">
--- app/home/@carousel/page.jsx
+++ app/home/@carousel/page.jsx
@@ -1,7 +1,28 @@
+
+const DATA = [
+  {
+    coverImageUri: 'https://user-images.githubusercontent.com/6414178/73920321-2357b680-4900-11ea-89d5-2e8cbecec9f6.jpg',
+  },
+  {
+    coverImageUri: 'https://user-images.githubusercontent.com/6414178/73920358-336f9600-4900-11ea-8eec-cc919b991e90.jpg',
+  },
+  {
+    coverImageUri: 'https://user-images.githubusercontent.com/6414178/73927874-25744200-490d-11ea-940f-db3e5dbd8b2b.jpg',
+  },
+  {
+    coverImageUri: 'https://user-images.githubusercontent.com/6414178/73920399-45e9cf80-4900-11ea-9d5b-743fe5e8b9a4.jpg',
+  },
+];
+
 export default function page() {
   return (
-    <main className="mt-12  mx-5">
-      <div className="aspect-[16/9] w-full bg-black"/>
+    <main className="space-x-2">
+      <div className=" flex justify-start items-center overflow-hidden py-14 overflow-x-scroll  no-scrollbar  snap-x snap-mandatory ">
+        {DATA.map((item, index) =>
+          <div key={index} style={{ backgroundImage: `url(${item.coverImageUri})` }} className="aspect-[16/9] shadow-xl shadow-gray-400 mx-4 min-w-[calc(100vw_-_40px)]  object-cover object-center bg-cover rounded-lg overflow-hidden snap-center" />
+        )}
+      </div>
     </main>
+
   )
 }
