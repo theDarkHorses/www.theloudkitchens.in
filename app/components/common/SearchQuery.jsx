80a997c654c0b91b20e3945fe6175dd758e31f2b
rohit gupta <lucidrohit@gmail.com>
2023-10-19 20:06:06 +0530

    added debounce in search and made cart bottom button

---
+++ app/components/common/SearchQuery.jsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { useRef, useEffect, useState, use } from "react";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useDebounce } from "@uidotdev/usehooks";
+import { searchCusine } from "@/app/queries/restaurant";
+import { extractDebouncedSearchQuery } from "@/app/utils/restaurant";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SearchQuery() {
+    const searchRef = useRef()
+    const [query, setQuery] = useState("")
+    const router = useRouter()
+    const [isFocused, setIsFocused] = useState(false)
+    const [searchResults, setSearchResults] = useState([])
+    const debouncedQuery = useDebounce(query, 200)
+
+    useEffect(() => {
+        searchRef.current?.focus()
+    }, [])
+
+    const handleFocus = () => {
+        setTimeout(() => {
+            setIsFocused(true)
+        }, 200)
+    }
+
+    const handleBlur = () => {
+        setTimeout(() => {
+            setIsFocused(false)
+        }, 200)
+
+    }
+
+    useEffect(() => {
+        const handleSearch = async () => {
+            try {
+                const res = await searchCusine(debouncedQuery)
+                setSearchResults(extractDebouncedSearchQuery(res))
+            } catch (err) {
+                console.log(err.message)
+            }
+        }
+        if (debouncedQuery) {
+            handleSearch()
+        }
+    }
+        , [debouncedQuery])
+
+
+    return (
+        <header className="border  relative rounded-lg  flex items-center py-2 px-2 mt-10 bg-white  shadow-md shadow-slate-200">
+            <ChevronLeft size={24} color="#AC2323" onClick={() => router.back()} className="cursor-pointer" />
+            <input
+                ref={searchRef}
+                className="px-2 leading-none font-lato text-base w-full h-full border-none outline-none"
+                placeholder="Search...."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
+            {isFocused && (
+                <div className="bg-white divide-y left-0 right-0 p-4 py-2 w-full absolute top-12 shadow-lg rounded-lg border">
+                    {searchResults.length ? searchResults.map((item) =>
+                        <Link onClick={(e) => e.stopPropagation()} href={`/restaurants/${item.restaurantId}?tab=${item.sectionId}&item=${item.itemId}`} key={item.itemId} className="flex py-4  gap-2">
+                            <Image src={item.itemImageUrl} alt={item.itemName} width={52} height={52}  className="w-12 h-12 rounded-lg overflow-hidden object-cover object-center"/>
+                            <div className="flex flex-col items-start justify-center">
+                                <h3 className="text-[#292C35] font-medium text-start font-lato text-lg ">
+                                    {item.itemName}
+                                </h3>
+                                <p className="font-lato text-xs font-medium text-[#A1A5A8] leading-none">
+                                    {item.restaurantName}
+                                </p>
+                            </div>
+                        </Link>
+                    ) : <p className="text-center text-text py-2">{!query.trim().length?"Search Something...":"No results found"}</p>
+
+                    }
+                </div>
+            )}
+        </header>
+    )
+}
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,9 +1,5 @@
-"use client"
-
-import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import SearchQuery from "../components/common/SearchQuery";
 
 const DATA = [
   {
@@ -30,21 +26,11 @@ const DATA = [
 
 
 export default function page() {
-  const [query, setQuery] = useState(null)
-  const router = useRouter()
-  const searchRef = useRef()
-
-  useEffect(() => {
-    searchRef.current?.focus()
-  }, [])
 
   return (
     <section className="px-5 h-[calc(100vh_-_64px)] bg-white  w-full overflow-hidden z-40">
-      <header className="border rounded-lg  flex items-center py-2 px-2 mt-10 bg-white  shadow-md shadow-slate-200">
-        <ChevronLeft size={24} color="#AC2323" onClick={() => router.back()} className="cursor-pointer" />
-        <input ref={searchRef} className=" px-2 leading-none font-lato  text-base w-full h-full border-none outline-none" placeholder="Search...." value={query} onChange={(e) => setQuery(e.target.value)} />
-      </header>
-      <main className="mt-3">
+      <SearchQuery />
+      {/* <main className="mt-3">
         <h1 className="uppercase  font-semibold pl-4 mt-10 font-lato  text-[#565D67] text-base">POPULAR ITEMS</h1>
         <div className="mt-4 ">
           {DATA.map((item, index) =>
@@ -60,7 +46,7 @@ export default function page() {
               </div>
             </div>)}
         </div>
-      </main>
+      </main> */}
     </section>
   )
 }
